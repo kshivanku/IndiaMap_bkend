15711f40b26b63d2cd31992edca77362373d7429
kshivanku <kshivanku@gmail.com>
2017-03-19 19:35:54 -0400

    data parser can run independently everyday and gather data for all villages/cities in India

--- dataParser.js
+++ dataParser.js
@@ -3,54 +3,99 @@ var XLSX = require('xlsx');
 var googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyDMyPpU0f9sxH9NXwc9-vpIlzJiDdWRi2I'
 });
-var csvWriter = require('csv-write-stream');
+var max_api_calls = 100;
 
-var file_name = [];
-var places_names = [];
+//LOADING ALL THE DATA IN PLACEDATA VARIABLE
+var latlong_data = JSON.parse(fs.readFileSync("output.json"));
+var session_data = []; //THIS IS MOSTLY TO TRACK THE NUMBER OF API CALLS, MANY CALLS COMEBACK WITH NO DATA THATS WHY THIS IS REQUIRED
+var max_row;
+var max_filename;
 
 //READ ALL THE FILES AVAILABLE IN DATA FOLDER AND STORE THEIR NAMES IN AN ARRAY
+var file_name = [];
 const dataFolder = './raw/';
 fs.readdir(dataFolder, (err, files) => {
   files.forEach(file => {
     file_name.push(file);
   });
-  parseData();
+  calibrate();
 })
 
-function parseData(){
+//CLIBERATE FUNCTION BASICALLY SETS WHICH FILE HAS TO BE READ FROM WHICH ROW
+function calibrate(){
+  var calibration_data = JSON.parse(fs.readFileSync("calibrate.json"));
+  var last_row = calibration_data.last_row;
+  var places_names = calibration_data.places;
+  var last_file = calibration_data.last_file;
+  var index;
+  if(last_file != "NULL"){
+    index = file_name.indexOf(last_file);
+  }
+  else{
+    index = 0;
+  }
+  if(last_row == "NULL"){
+    last_row = 2;
+  }
+  parseData(index, last_row, places_names);
+}
+
+function parseData(index, last_row, places_names){
+  var api_call_num = 0;
   //READ EVERY FILE
-  for (i = 0 ; i < file_name.length ; i++){
+  for (i = index ; i < file_name.length ; i++){
+    max_filename = file_name[i];
     var workbook = XLSX.readFile('raw/' + file_name[i]);
     var first_sheet = workbook.Sheets[workbook.SheetNames[0]];
     var total_rows = Number(first_sheet['!ref'].split(':')[1].replace(/^\D+/g, ''));
     //READ EVERY COLUMN OF FILE i
-    for (row = 2 ; row <= total_rows ; row++){
+    for (row = last_row ; row <= total_rows; row++){
+      max_row = row;
       var level_cell = 'G' + row;
       var name_cell = 'H' + row;
       var level = first_sheet[level_cell].v;
       var name = first_sheet[name_cell].v + ', ' + file_name[i].split(".")[0];
       if (places_names.indexOf(name) == -1){
         places_names.push(name);
-        findgeolocation(name, level);
+        api_call_num += 1;
+        findgeolocation(name, level, places_names);
+      }
+      if(api_call_num >= max_api_calls){
+        return 1; //STOP MAKING API CALLS IF MAX NUMBER IS REACHED
       }
     }
+    last_row = 2; //RESETING THE ROW NUMBERS WHEN NEXT FILE HAS TO BE READ
   }
 }
 
-function findgeolocation(name, level){
+function findgeolocation(name, level, places_names){
+  var data = {
+    name: name,
+    level: level
+  }
   googleMapsClient.geocode({
     address: name
   }, function(err, response) {
     if (!err) {
-      // var latlong = JSON.stringify(response.json.results, null, 2);
-      // fs.writeFileSync("latlong.json", latlong);
-      var ne_lat = response.json.results[0].geometry.bounds.northeast.lat;
-      var ne_lng = response.json.results[0].geometry.bounds.northeast.lng;
-      var sw_lat = response.json.results[0].geometry.bounds.southwest.lat;
-      var sw_lng = response.json.results[0].geometry.bounds.southwest.lng;
-      var lat = response.json.results[0].geometry.location.lat;
-      var lng = response.json.results[0].geometry.location.lng;
-      writeToOutput(name, level, ne_lat, ne_lng, sw_lat, sw_lng, lat, lng);
+      if(response.json.results[0]){
+        if(response.json.results[0].geometry.bounds){
+          data.ne_lat = response.json.results[0].geometry.bounds.northeast.lat;
+          data.ne_lng = response.json.results[0].geometry.bounds.northeast.lng;
+          data.sw_lat = response.json.results[0].geometry.bounds.southwest.lat;
+          data.sw_lng = response.json.results[0].geometry.bounds.southwest.lng;
+        }
+        data.lat = response.json.results[0].geometry.location.lat;
+        data.lng = response.json.results[0].geometry.location.lng;
+        latlong_data.push(data);
+        session_data.push(data);
+      }
+      else {
+        session_data.push("BLANK"); //JUST TO KEEP THE COUNT RIGHT WHEN NO DATA IS RECEIVED FOR A PARTICULAR CALL
+      }
+      if(session_data.length >= max_api_calls){
+        console.log("saving changes");
+        saveChanges(places_names);
+      }
     }
     else {
       console.log(err);
@@ -58,16 +103,14 @@ function findgeolocation(name, level){
   });
 }
 
-var file_empty = true;
-function writeToOutput(name, level, ne_lat, ne_lng, sw_lat, sw_lng, lat, lng){
-  if(file_empty){
-    var writer = csvWriter();
-    file_empty = false;
+function saveChanges(places_names_covered){
+  var dataToCalibrate = {
+    'last_file': max_filename,
+    'last_row': max_row,
+    'places': places_names_covered
   }
-  else{
-    var writer = csvWriter({sendHeaders: false});
-  }
-  writer.pipe(fs.createWriteStream('output.csv', {'flags': 'a'}));
-  writer.write({Name: name, Level:level, NE_LAT:ne_lat, NE_LNG:ne_lng, SW_LAT:sw_lat, SW_LNG:sw_lng, LAT:lat, LNG:lng});
-  writer.end();
+  var dataToCalibrate_str = JSON.stringify(dataToCalibrate, null, 2);
+  fs.writeFileSync("calibrate.json", dataToCalibrate_str);
+  var latlong_data_srt = JSON.stringify(latlong_data, null, 2);
+  fs.writeFileSync("output.json", latlong_data_srt);
 }
